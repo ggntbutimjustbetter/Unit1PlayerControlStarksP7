81cc34fc7cccfe7bd299541d9e979f404423668f
Tristan Starks <757637@student.dusd.net>
2024-03-12 15:12:28 -0700

    DONE
    done done doneeeeee

--- Unit1PlayerControlP7Starks/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlP7Starks/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 20.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
@@ -23,5 +26,11 @@ public class PlayerController : MonoBehaviour
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
